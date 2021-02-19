5cb3ec85f9fe3b311da797bf57e943b88265708e
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-19 12:46:28 -0800

    refactor as an example of a closure

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,22 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!(
-        "Running slow calculation. Approximately {} seconds...",
-        intensity
-    );
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    // this holds the definition of an anonymous function, not its result
+    let expensive_closure = |num| {
+        println!("Calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    }
+
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result);
-        println!("Next, do {} situps!", expensive_result);
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", expensive_result);
+            println!("Today, run for {} minutes!", expensive_closure(intensity));
         }
     }
 }
